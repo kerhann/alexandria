d9f06943ccb5176a3cd97b92900b9252fb3cfa25
kerhann <jferet@gmail.com>
2015-11-04 17:42:23 +0100

    Initial commit

---
+++ app/src/main/java/it/jaschke/alexandria/ScannerActivity.java
@@ -0,0 +1,51 @@
+package it.jaschke.alexandria;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import me.dm7.barcodescanner.zbar.BarcodeFormat;
+import me.dm7.barcodescanner.zbar.Result;
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
+
+public class ScannerActivity extends AppCompatActivity implements ZBarScannerView.ResultHandler {
+    private ZBarScannerView mScannerView;
+
+    @Override
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        mScannerView = new ZBarScannerView(this);    // Programmatically initialize the scanner view
+        List<BarcodeFormat> formats = new ArrayList<BarcodeFormat>();
+        formats.add(BarcodeFormat.EAN13);
+        mScannerView.setFormats(formats);
+        setContentView(mScannerView);                // Set the scanner view as the content view
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Log.v("ICI", rawResult.getContents()); // Prints scan results
+        Log.v("ICI", rawResult.getBarcodeFormat().getName()); // Prints the scan format (qrcode, pdf417 etc.)
+        Intent addBookAfterScan = new Intent(this, MainActivity.class);
+        addBookAfterScan.putExtra("ISBN", rawResult.getContents());
+        this.startActivity(addBookAfterScan);
+
+    }
+}
